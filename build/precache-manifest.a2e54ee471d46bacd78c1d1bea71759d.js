31e2f07cdbf67a24c648bc73a7d993067bc56f8c
Antahai <kpuisto@gmail.com>
2020-10-04 00:34:25 +0300

    frontend build added

---
+++ build/precache-manifest.a2e54ee471d46bacd78c1d1bea71759d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd74b2e1ab8172ef2a977734216f5f92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ca408be486f7b231dc",
+    "url": "/static/css/main.57183777.chunk.css"
+  },
+  {
+    "revision": "e41e4cc4897c68aa283a",
+    "url": "/static/js/2.6e6eecea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e6eecea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ca408be486f7b231dc",
+    "url": "/static/js/main.ac380c03.chunk.js"
+  },
+  {
+    "revision": "194d2a893d70517d6cc2",
+    "url": "/static/js/runtime-main.8f096503.js"
+  }
+]);
